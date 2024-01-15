5a3bb3a8f90ad16bb239dae7f9f36a4ed1d6f396
Vladimir <202213870k@utea.edu.pe>
2024-01-15 11:22:59 -0500

    primer commit

--- SalidaFormateada01.java
+++ SalidaFormateada01.java
@@ -1,7 +1,7 @@
 public class SalidaFormateada01 {
     public static void main(String[] args) {
-        System.out.println("Disyunciòn inclusiva o debil: P v Q se lee: P o Q");
-        System.out.println("Condicional: P -> Q se lee: si P entonces Q");
-        System.out.println("negaciòn: la negacion de P es -P se lee: no es cierto que P");
+        System.out.printf("El número %d no tiene decimales.\n", 21);
+        System.out.printf("El número %f sale con decimales.\n", 21.0);
+        System.out.printf("El %.3f sale exactamente con 3 decimales.\n", 21.0);
     }
 }
