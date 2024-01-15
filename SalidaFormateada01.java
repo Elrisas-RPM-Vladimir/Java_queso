31b230a76a2de85f25b930ac7f5ed3be2c6c8140
Vladimir <202213870k@utea.edu.pe>
2024-01-15 11:16:16 -0500

    primer commit

---
+++ SalidaFormateada01.java
@@ -0,0 +1,7 @@
+public class SalidaFormateada01 {
+    public static void main(String[] args) {
+        System.out.println("Disyunciòn inclusiva o debil: P v Q se lee: P o Q");
+        System.out.println("Condicional: P -> Q se lee: si P entonces Q");
+        System.out.println("negaciòn: la negacion de P es -P se lee: no es cierto que P");
+    }
+}
