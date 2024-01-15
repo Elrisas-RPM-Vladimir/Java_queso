00cd41e0cf072ca3ff5bc36300cd686c203e4960
Vladimir <202213870k@utea.edu.pe>
2024-01-15 10:40:23 -0500

    otra Commit

---
+++ CaracteresEspeciales.java
@@ -0,0 +1,6 @@
+public class CaracteresEspeciales {
+    public static void main(String[] args){
+        System.out.println("Cuando programo ando queso 🧀ヽ（≧□≦）ノ");
+        System.out.println("No voy a comer, voy a cenar comiendo -Diego. D");
+    }
+}
