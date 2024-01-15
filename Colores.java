5acd56513223c58dbd03d43ae033dd3883ddafe9
Vladimir <202213870k@utea.edu.pe>
2024-01-15 09:45:44 -0500

    Primer Commit

---
+++ Colores.java
@@ -0,0 +1,11 @@
+public class Colores {
+    public static void main(String[] args) {
+        System.out.print("\033[90m tengo hambre wey");
+        System.out.print("\033[91m hola tonotos");
+        System.out.print("\033[92m fiesta factory");
+        System.out.print("\033[35m poder polìtico");
+        System.out.print("\033[36m somos amiguis");
+        System.out.print("\033[37m pertubaDross");
+        System.out.print("\033[92m Chat GPT");
+        }
+        }
