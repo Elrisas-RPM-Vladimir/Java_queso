de7978e0c2620339e82e6bd0d4dcdc772ac6d46b
Vladimir <202213870k@utea.edu.pe>
2024-01-16 13:12:42 -0500

    ejercicio commit

--- EjerciciosDeProgramación/Ejercicio3.java
+++ EjerciciosDeProgramación/Ejercicio3.java
@@ -2,6 +2,26 @@ package EjerciciosDeProgramación;
 
 public class Ejercicio3 {
     public static void main(String[] args) {
-        
+        System.out.println("Canción: Waiting For Love - Avicii");
+        System.out.println("┌──────────────────────────────────────────────────────────────┬─────────────────────────────────────────────────────────────┐");
+        System.out.println("│                               Español                        │                              Inglés                         │");
+        System.out.println("├──────────────────────────────────────────────────────────────┼─────────────────────────────────────────────────────────────┤");
+        System.out.print("│\033[90m El lunes me dejo roto \033[39;49m                                       │");
+        System.out.println("\033[90m Monday left me broken \033[39;49m                                      │");
+        System.out.print("│\033[31m El martes, terminé con la esperanza. \033[39;49m                        │");
+        System.out.println("\033[91m Tuesday, I was through with hoping \033[39;49m                         │");
+        System.out.print("│\033[32m Miércoles, mis brazos vacíos estaban abiertos \033[39;49m               │");
+        System.out.println("\033[92m Wednesday, my empty arms were open \033[39;49m                         │");
+        System.out.print("│\033[33m Jueves, esperando el amor, esperando el amor. \033[39;49m               │");
+        System.out.println("\033[93m Thursday, waiting for love, waiting for love \033[39;49m               │");
+        System.out.print("│\033[34m Gracias a las estrellas, es viernes. \033[39;49m                        │");
+        System.out.println("\033[94m Thank the stars, it's Friday \033[39;49m                               │");
+        System.out.print("│\033[35m Estoy ardiendo como un fuego enloquecido el sábado \033[39;49m          │");
+        System.out.println("\033[95m I'm burning like a fire gone wild on Saturday \033[39;49m              │");
+        System.out.print("│\033[36m Supongo que no iré a la iglesia el domingo. \033[39;49m                 │");
+        System.out.println("\033[96m Guess I won't be coming to church on Sunday \033[39;49m                │");
+        System.out.print("│\033[37m Estaré esperando el amor, esperando que el amor llegue. \033[39;49m     │");
+        System.out.println("\033[97m I'll be waiting for love, waiting for love to come around \033[39;49m  │");
+System.out.println("└──────────────────────────────────────────────────────────────┴─────────────────────────────────────────────────────────────┘");
     }
 }
---
+++ EjerciciosDeProgramación/Ejercicio4.java
@@ -0,0 +1,18 @@
+package EjerciciosDeProgramación;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        System.out.println("┌──────────────┬──────────────┬──────────────┬──────────────┬──────────────┐");
+        System.out.println("│   Lunes      │  Martes      │ Miércoles    │   Jueves     │  Viernes     │");
+        System.out.println("├──────────────┼──────────────┼──────────────┼──────────────┼──────────────┤");
+        System.out.println("│ Algoritmos y │ Algoritmo y  │  Ingieneria  │  Ingieneria  │ Estadística  │");
+        System.out.println("│Programación 1│Programación 1│  de Sistemas │  de Sistemas ├──────────────┤");
+        System.out.println("├──────────────┼──────────────┼──────────────┼──────────────┤  Calculo 1   │");
+        System.out.println("│  Economía    │  Calculo 1   │ Estadística  ├──────────────┼──────────────┤");
+        System.out.println("├──────────────┼──────────────┼──────────────┼──────────────┼──────────────┤");
+        System.out.println("│  Matemática  │  Matemática  │   Economía   ├──────────────┼──────────────┤");
+        System.out.println("│   Discreta   │   Discreta   ├──────────────┼──────────────┼──────────────┤");
+        System.out.println("├──────────────┼──────────────┼──────────────┼──────────────┼──────────────┤");
+        System.out.println("└──────────────┴──────────────┴──────────────┴──────────────┴──────────────┘");
+    }
+}
