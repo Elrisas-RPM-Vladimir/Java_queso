20228402060879ccb4d9a5702dcd9c0eef2f181d
Vladimir <202213870k@utea.edu.pe>
2024-01-23 12:32:41 -0500

    ejercicios commit

---
+++ EjerciciosDeVariables/EjercicioVariable1.java
@@ -0,0 +1,35 @@
+package EjerciciosDeVariables;
+
+public class EjercicioVariable1 {
+    /**
+     * @param args
+    /** */
+    public static void main(String[] args) {
+        int x;  // Declara la variable x como entero
+
+        x = 144; // Asignar el valor 144 a la variable x
+
+        // Muestra el valor de x
+
+        int y; // Declarar la variable y como entero
+
+        y = 999; // Asignar el valor 999 a la variable y
+
+        // Muestra el valor de y
+
+        System.out.println(x);
+        System.out.println(y);
+        /*
+         * Operadores aritmeticos
+         * + suma
+         * - resta
+         * * multiplica
+         * / división
+         */
+        System.out.println(x +" + "+ y +" = "+ (x + y));
+        System.out.println(x +" - "+ y +" = "+ (x - y));
+        System.out.println(x +" * "+ y +" = "+ (x * y));
+        System.out.println(x +" / "+ y +" = "+ (x / y));
+        System.out.println("___________________________");
+    }
+}
