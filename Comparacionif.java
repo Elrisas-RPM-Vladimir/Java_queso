42b4b397663f2697d3993ea1ba5ed636491ae3d7
Vladimir <202213870k@utea.edu.pe>
2024-01-30 11:56:59 -0500

    ejercicio commit

---
+++ Comparacionif.java
@@ -0,0 +1,28 @@
+public class Comparacionif {
+    public static void main(String[] args) {
+        /*
+         * sentencia condicional, permitir al programa bifurcar
+         * el flujo de la ejecución del programa pendiente
+         * de una condicion (verdad o falso)
+         * 
+         * Sentencia if
+         * 
+         * if(condicion){
+         *    instrucciones a ejecutar si la condicion es verdadera
+         * }else{
+         *    instrucciones a ejecutar si la condicion es falso
+         * }
+         * 
+         */
+
+         String miFruta = "naranja";
+
+         if (miFruta == "naranja") {
+            System.out.println("Que rica fruta, me la como!!!");
+        }else{
+            System.out.println("No puedo comer esa fruta!!!");
+         }
+
+         //Programa que indique cual es la capital de Perú
+    }
+}
