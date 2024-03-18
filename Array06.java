e56f186bec666473bea5386068f663348bdace11
Vladimir <202213870k@utea.edu.pe>
2024-03-18 10:44:55 -0500

    ejercicio commit

---
+++ Array06.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Random;
+public class Array06 {
+    /*Realizar un programa que sea capaz de ordenar un array de una 
+    dimención, que contenga 20 numeros enteros aleatorios entre 0 y 200 
+    */
+    public static void main(String[] args) {
+        Random r = new Random();
+        int[] numeros = new int[20];
+        //llenamos el arreglo
+        {System.out.println("20 números aleatorios entre 0 y 200");
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = (int) (Math.random()*201);
+        }
+        }
+        //Mostrar los valores del arreglo
+        System.out.println("\nArray no ordenado:");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i] +"\t");
+        }
+        //Ordenamiento
+        System.out.println("\nArray ordenado:");
+        Arrays.sort(numeros);
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i] +"\t");
+        }
+        int menor = numeros[0];
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] < menor) {
+            } else {
+                if (numeros[i] > menor) {
+                    menor = menor;
+                }
+            }
+        }
+
+    }
+}
+
