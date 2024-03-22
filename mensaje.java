d1a3a509017961d7211caf1c59d15bfa06fd84ed
Elrisas-RPM-Vladimir <202213870k@utea.edu.pe>
2024-03-21 22:36:01 -0500

    commit

---
+++ mensaje.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class mensaje {
+    public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+        System.out.print("Ingresa tu edad: ");
+        String edad = s.next();
+        System.out.println("Saludos profesora Yuliana, tarde mucho pero andaba");
+        System.out.println("ocupado asi que le saludo por un:");
+        System.out.println("  ____||____        FELIZ CUMPLEAÑOS        |");
+        System.out.println("////////////| PROFESORA: Tomaylla Gutierrez |");
+        System.out.println("|    _    | |            Yuliana Miriam     |");
+        System.out.println("|[] | | []|_|      EDAD:"+ edad +"                  |");
+        System.out.println("|   |─|   | /                               |");
+        System.out.println("└─────────┘/    que este dia lo pase bonito |");
+        System.out.println("   |   |        con sus familiares y que    |");
+        System.out.println("  |#   #|       pase un feliz día.          |");
+        System.out.println(" |#  ~  #|                                  |");
+        System.out.println("|_#_____#_|     BUENAS NOCHES MISS :)       |");
+    }
+}
