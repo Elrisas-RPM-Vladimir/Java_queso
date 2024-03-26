521df50cac06578de63df4f92b921b5f96e62d19
Elrisas-RPM-Vladimir <202213870k@utea.edu.pe>
2024-03-26 09:40:29 -0500

    Expo commit

--- Seleccionselection.java
+++ Seleccionselection.java
@@ -1,28 +1,40 @@
 public class Seleccionselection {
     public static void main(String[] args) {
              
-        int prueba[] = new int[]{8,2,3,5,0,1,9,12};
+        int numeros[] = new int[]{8,2,3,20,5,0,1,9,12,14,13,15,10,19,18,11,16,17};
+        //Declaración del array que en este caso sera un Array unidimencional
 
-        for(int dato:prueba){
-            System.out.print(dato+",");
-        }
-        
-        for(int contadorOrdenar = 0;contadorOrdenar < prueba . length -1; contadorOrdenar++){
-            int valorMinimo = contadorOrdenar;
-            for(int contadorComparar = contadorOrdenar+1; contadorComparar < prueba.length; contadorComparar++){
-                if(prueba[contadorComparar] < prueba[valorMinimo]){
-                    valorMinimo = contadorComparar;
-                }
+        for(int dato:numeros){
+            System.out.print(dato+", ");
+        } //Con for empezamos a generar los datos puestos por el array
+
+
+        //Aqui empezamos con el Algoritmo de selección(SelectionSort), para empezar a seleccionar a los numeros,
+        //de menor a mayor.
+        for(int Ordenar = 0; Ordenar < numeros.length -1; Ordenar++){ 
+            //Creamos el valor ordenar para empezar el comando selection, por el cual creamos el Minimo
+            int Minimo = Ordenar; //para empezar a recorre el Array
+            for(int comp = Ordenar+1; comp < numeros.length; comp++){
+                //el comp(comparar) será quien seleccione los valores menores del array para
+                //seleccionar los números para su respectiva clasificación
+                if(numeros[comp] < numeros[Minimo]){
+                    Minimo = comp;
+                } 
+                //hacemos que la condición if seleccione los numeros con comp para cumplir con
+                //que los numeros se ordenen de manera ascendente.
             }
-            int auxiliar = prueba[contadorOrdenar];
-            prueba[contadorOrdenar] = prueba[valorMinimo];
-            prueba[valorMinimo]=auxiliar;
+            int auxiliar = numeros[Ordenar];
+            numeros[Ordenar] = numeros[Minimo];
+            numeros[Minimo] = auxiliar;
+            //El int auxiliar generara los números de manera ordenada de acuerdo a los datos que
+            //le indica el SelectionSort
         }
-
-    System.out.println("");
-        for(int dato:prueba){
-            System.out.print(dato +",");
-        }
+        System.out.println("");
+        System.out.println("Datos ordenados:");
+        for(int dato:numeros){
+        System.out.print(dato +", ");
+        } //Después de ordenar el array, este bloque de código imprime los datos nuevamente,
+        //pero esta vez después de que han sido ordenados. Para evitar conflictos de código
     }
     
 }
